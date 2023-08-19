a75b164bb3214d1cf6ab514eb2733464473d52af
Wilkenson Hilarion <wilkenson@BookPro.local>
2023-08-19 18:33:46 -0400

    models

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from dotenv import load_dotenv
 from website import config
-
+#  users
 from .server.views import view
 from .server.auths import auth
 # admin
 from .server.admin_views import admin_view
 from .server.admin_auths import admin_auth
+
+from flask_sqlalchemy import SQLAlchemy
 
 import os
 
@@ -23,13 +25,18 @@ for filename in os.listdir(config_dir):
 
 SECRET_KEY = os.getenv('SECRET_KEY')  # set secret key
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__, template_folder='client/templates', static_folder='client/static')
     app.config['SECRET_KEY'] = SECRET_KEY  # set secret key
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    # user route
     app.register_blueprint(view, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    # admin route
     app.register_blueprint(admin_view, url_prefix='/')
     app.register_blueprint(admin_auth, url_prefix='/')
 
--- server/auths.py
+++ server/auths.py
@@ -4,16 +4,35 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash,
 auth = Blueprint('auth', __name__)
 
 
+def validate_credential(email=None, password=None):
+    validate_email = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"  # pattern validate email
+    validate_psswrd = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"  # validate pass
+    if email and not password:
+        return re.match(validate_email, email)
+    elif password and not email:
+        return re.match(validate_psswrd, password)
+
+
 @auth.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email == '':
+            flash("Email field cannot be empty", category="warning")
+        elif password == '':
+            flash("Password field cannot be empty", category="warning")
+        elif not validate_credential(email, None):
+            flash("Invalid Email", category="error")
+        elif not validate_credential(None, password):
+            flash("Wrong password", category="success")
+
     return render_template('login.html')
 
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
-    validate_email = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"  # pattern validate email
-    validate_psswrd = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"  # validate pass
-
     if request.method == "POST":
 
         first_name = request.form.get('first_name')
@@ -22,14 +41,14 @@ def register():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')  # confirm password
 
-        if not re.match(validate_email, email):
+        if not validate_credential(email, None):
             flash('Invalid email -> examplename@gmail.com', category="error")
         elif password1 != password2:
             flash('Password do not match Fild One and Field Two must be the same', category="error")
-        elif not re.match(validate_psswrd, password1):
+        elif not validate_credential(None, password1):
             flash("""Invalid password: ->  
             At least 8 characters long 
-            Contains at least one special character (e.g., !, @, #, $, etc.) and number"""
+            Contains at least one special character (e.g., !, @, #, $, etc.) and at least a number"""
                   , category="error")
         else:
             flash('Registered Successfully', category="success")
