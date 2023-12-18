d8b867cc105c6c30fc9d45e52778bc20a2142d17
Wilkenson <tcbwilkenson@gamilcom>
2023-12-18 17:45:05 -0500

    update login and signup page, endpoint

--- app/server/auths.py
+++ app/server/auths.py
@@ -2,14 +2,14 @@ import re
 from flask import Blueprint, render_template, redirect, url_for, request, flash, get_flashed_messages
 
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+import random
 
 auth = Blueprint('auth', __name__)
 
 
 def validate_credential(email=None, password=None):
-    # pattern validate email
-    validate_email = r"^[A-a-zZ0-9.%_+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    # validate pass
+    validate_email = r"^[a-zA-Z0-9.%_+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     validate_psswrd = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
     if email and not password:
         return re.match(validate_email, email)
@@ -24,13 +24,30 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         if email == '':
-            flash("Email field cannot be empty", category="warning")
+            flash("Email field cannot be empty", category="error")
+            return
         elif password == '':
-            flash("Password field cannot be empty", category="warning")
+            flash("Password field cannot be empty", category="error")
+            return
         elif not validate_credential(email, None):
             flash("Invalid Email", category="error")
         elif not validate_credential(None, password):
-            flash("Wrong password", category="success")
+            flash("Wrong password", category="error")
+            return
+        else:
+            from .models import User
+            user = User.query.filter_by(email=email).first()
+            if user:
+                if check_password_hash(user.password1, password):
+                    login_user(user, remember=True)
+
+                    return redirect(url_for('view.issues'))
+                else:
+                    flash("Wrong password", category="error")
+                    return redirect(url_for('auth.login'))
+            else:
+                flash("Email does not exist create an account ", category="error")
+                return redirect(url_for('auth.register'))
 
     return render_template('login.html')
 
@@ -42,28 +59,31 @@ def register():
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
         email = request.form.get('email')
+        username = request.form.get('username')
         password1 = request.form.get('password1')
-        password2 = request.form.get('password2')  # confirm password
+        password2 = request.form.get('password2')
 
-        if not validate_credential(email, None):
+        if not validate_credential(email=email):
             flash('Invalid email -> examplename@gmail.com', category="error")
         elif password1 != password2:
             flash(
-                'Password do not match Fild One and Field Two must be the same', category="error")
-        elif not validate_credential(None, password1):
+                'Password must be the same', category="error")
+        elif not validate_credential(password=password1):
             flash("""Invalid password: ->  
             At least 8 characters long 
             Contains at least one special character (e.g., !, @, #, $, etc.) and at least a number""", category="error")
         else:
             from .models import User
-            from website import db
+            from app import db
+            # need to check if email already exist
+            # need to check if username already exist
+            # email verification
 
             new_user = User(first_name=first_name, last_name=last_name, email=email,
                             password1=generate_password_hash(
                                 password1, method='sha256'),
-                            password2=password2)
+                            username=username)
 
-            #  add new user to db
             db.session.add(new_user)
             db.session.commit()
 
@@ -74,5 +94,5 @@ def register():
 
 @auth.route('/logout')
 def logout():
-    flash("logout success.", category="success")
+    logout_user()
     return redirect(url_for('view.home'))
